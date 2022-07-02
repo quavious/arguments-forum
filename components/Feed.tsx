cac580ab53fd25108a579bc99812fcc05cc5c6fe
quavious <nw.lee@outlook.com>
2022-07-03 01:38:44 +0900

    fix: feed style

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { FeedModel } from '../model/feed';
 import style from './Feed.module.scss';
 
@@ -13,12 +14,14 @@ const Feed = ({ feed }: { feed: FeedModel }) => {
   };
   return (
     <div className={style['feed']}>
-      <div className="flex items-center mt-4 sm:mt-2">
+      <div className="flex items-center mt-2 sm:mt-2">
         <h5 className={style['username']}>{feed.fakeUsername}</h5>
         <h5 className={style['createdAt']}>{compareDate(feed.createdAt)}</h5>
       </div>
-      <p className={style['content']}>{feed.content}</p>
-      <h5 className=" text-blue-900 font-bold">{feed.subFeed} 서브피드</h5>
+      <p className={style['content']}>
+        <Link href={`/feeds/${feed.id}`}>{feed.content}</Link>
+      </p>
+      <h5 className="mt-2 text-blue-900 font-bold">{feed.subFeed} 서브피드</h5>
       <hr className={style['divider']} />
       <a
         href={feed.newsLink}
