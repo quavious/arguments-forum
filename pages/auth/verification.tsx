5162a3f8e0a1fd077c985c0fb3b07a0fb2c97ad6
quavious <nw.lee@outlook.com>
2022-07-02 14:24:40 +0900

    feat: redirect after login

--- pages/auth/verification.tsx
+++ pages/auth/verification.tsx
@@ -1,6 +1,19 @@
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import Verification from '../../components/Verification';
 
 const VerificationPage = () => {
+  const router = useRouter();
+  const { status } = useSession();
+  useEffect(() => {
+    if (!router) {
+      return;
+    }
+    if (status === 'authenticated') {
+      router.push('/');
+    }
+  }, [router, status]);
   return <Verification />;
 };
 
